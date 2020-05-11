5842c54b0afdb6bc66b0e81ec95309adb4e4ec8f
wesley <43890908+wbenica@users.noreply.github.com>
2020-05-11 13:09:42 -0700

    tables output vertically or horizontally depending on output row/column values

--- dataframes.py
+++ dataframes.py
@@ -27,45 +27,58 @@ def get_track_df(q, track_var):
 
     if not aggregation:
         if counties:
-            df = get_unagg_track_df(q.data, track_var, counties=counties)
+            df = get_unagg_track_df(q, track_var, counties=counties)
         else:
-            df = get_unagg_track_df(q.data, track_var, states=states)
+            df = get_unagg_track_df(q, track_var, states=states)
     else:
         if counties:
-            df = get_agg_track_df(q.data['data'], target=target[0])
+            df = get_agg_track_df(q, target=target[0])
         else:
-            df = get_agg_track_df(q.data['data'], target=aggregation)
+            df = get_agg_track_df(q, target=aggregation)
 
     return df
 
 
-def get_unagg_track_df(data: dict, track_var: str, counties=None, states=None):
-    dates = [int_to_date(datum['date']) for datum in data['data']]
+def get_unagg_track_df(q: Query, track_var: str, counties=None, states=None):
+    data = q.data
 
-    if counties:
-        df = pd.DataFrame(index=dates, columns=counties)
-    elif states:
-        df = pd.DataFrame(index=dates, columns=states)
+    if q.output['table']['column'] == 'time':
+        idx = [int_to_date(datum['date']) for datum in data['data']]
+        col = counties if counties else states
+        is_vert = True
     else:
-        raise ValueError
+        idx = counties if counties else states
+        col = [int_to_date(datum['date'], short=True) for datum in data['data']]
+        is_vert = False
+
+    df = pd.DataFrame(index=idx, columns=col)
 
     for col in df.columns:
         df[col].values[:] = 0
 
     field = 'county' if counties else 'state'
+    df.index.name = None if is_vert else field
+    df.columns.name = field if is_vert else None
     for d in data['data']:
         for dd in d['daily_data']:
-            df[dd[field]][int_to_date(d['date'])] = dd.get(track_var) or 0
-
+            if is_vert:
+                df[dd[field]][int_to_date(d['date'])] = dd.get(track_var) or 0
+            else:
+                df[int_to_date(d['date'], short=True)][dd[field]] = dd.get(track_var) or 0
     return df
 
 
-def get_agg_track_df(data: dict, target):
+def get_agg_track_df(q: Query, target):
+    data = q.data['data']
+    is_vert = q.output['table']['row'] == 'time'
     # TODO add target as a title?
     df = pd.DataFrame(data)
-    df['date'] = df['date'].apply(lambda date_int: int_to_date(date_int))
-    return df.set_index('date')
-
+    df['date'] = df['date'].apply(lambda date_int: int_to_date(date_int, short=is_vert))
+    df = df.set_index('date')
+    df.index.name = None
+    if is_vert:
+        df = df.transpose()
+    return df
 
 def get_stats_df(q):
     return pd.DataFrame()
