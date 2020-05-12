be72481f81938d3f4202311a271188ffae5191dc
wesley <43890908+wbenica@users.noreply.github.com>
2020-05-11 18:17:34 -0700

    bar graph works to an extent. mainly layout/spacing issues. Need labels and titles

--- output_graph.py
+++ output_graph.py
@@ -1,3 +1,4 @@
+import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib.ticker import MultipleLocator
 
@@ -10,22 +11,29 @@ def create_graph(q: Query):
     legend = q.output['graph'].get('legend') == 'on'
     combo = q.output['graph']['combo']
     title = q.output['graph'].get('title')
+    df = get_df(q)
 
     fig, ax = plt.subplots()
 
     if graph_type == 'bar':
         grapher = ax.bar
+        labels = np.arange(len(df.index))
+        width = 1
+        for i, col in enumerate(df.columns):
+            offset = i * width / len(df.columns)
+            grapher(labels - width / 2 + offset, height=df[col], width=width / len(df.columns), label=col)
+        ax.set_xticklabels(df.index)
     elif graph_type == 'line':
         grapher = ax.plot
+        grapher(df)
+        ax.xaxis.set_major_locator(MultipleLocator(5))
     elif graph_type == 'scatter':
         grapher = ax.scatter
+        grapher(df)
     else:
         raise ValueError("Graph type must be 'bar', 'line', or 'scatter'")
 
-    df = get_df(q)
 
-    grapher(df)
-    ax.xaxis.set_major_locator(MultipleLocator(5))
 
     plt.xticks(rotation=90)
     if title:
